84a9b9c8f04d663077de29e5b1cab8e44c6d9dba
Xiaojinglin <Candylinxiaojing@gmail.com>
2022-02-14 07:42:12 -0500

    Add a service charge

--- MasterTicket/MasterTicket.py
+++ MasterTicket/MasterTicket.py
@@ -1,31 +1,33 @@
+service_charge = 2
 ticket_price = 10
 ticket_remaining = 100
-name = ''
-quantity = 0
-total = 0
-print(f"There is {ticket_remaining} ticket left!")
-name = input("Please enter your name: ")
-while True:
-    quantity = input(f"{name}, How many tickets would you like to purchase? ")
-    if not quantity.isnumeric():
-        print(f"You should enter a digit number")
-        continue
-    quantity = int(quantity)
-    if quantity>ticket_remaining:
-        print(f"We only have {ticket_remaining} tickets left.")
-        continue
-    else:
-        break
-total = ticket_price * quantity
-print(f"You have purchase {quantity} tickets, the total is ${total}.")
-while True:
-    enter = input(f"Enter y to confirm your order, c to cancel: ")
-    if enter == 'y':
-        print("Thank you for your purchase!")
-        break
-    elif enter == 'c':
-        print("Your order has been cancelled")
-        break
-    else:
-        enter = print("Invalid Input")
-        continue
+def amount(num_of_ticket):
+    return (ticket_price * num_of_ticket) + service_charge
+while ticket_remaining >= 1:
+    print(f"There is {ticket_remaining} ticket left!")
+    name = input("What's your name: ")
+    while True:
+        num_tickets = input(f"{name}, How many tickets would you like to purchase? ")
+        if not num_tickets.isnumeric():
+            print(f"You should enter a digit number")
+            continue
+        num_tickets = int(num_tickets)
+        if num_tickets > ticket_remaining:
+            print(f"We only have {ticket_remaining} tickets left.")
+            continue
+        else:
+            break
+    amount = amount(num_tickets)
+    print(f"You have purchase {num_tickets} tickets, the total is ${amount}.")
+    while True:
+        enter = input(f"Enter y to confirm your order, c to cancel: ")
+        if enter == 'y':
+            print("Thank you for your purchase!")
+            ticket_remaining -= num_tickets
+            break
+        elif enter == 'c':
+            print("Your order has been cancelled")
+            break
+        else:
+            enter = print("Invalid Input")
+            continue    
