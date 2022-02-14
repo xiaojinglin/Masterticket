9ab058c2423b62d1b352d07b2055ba865f51c4f8
Xiaojinglin <Candylinxiaojing@gmail.com>
2022-02-13 22:24:35 -0500

    Create a MasterTicket project

---
+++ MasterTicket/MasterTicket.py
@@ -0,0 +1,8 @@
+ticket_price = 10
+ticket_remaining = 100
+name = ''
+quantity = 0
+print(f"There is {ticket_remaining} ticket left!")
+name = input("Please enter your name: ")
+quantity = print("Hi, {name}! How many tickets would you like to purchase? ")
+if quantity.is
